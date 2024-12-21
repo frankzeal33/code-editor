45ca7de1d335c654f77f768f8d307c3879633bff
frankzeal33 <frankzeal33@gmail.com>
2024-12-21 18:23:24 +0100

    final commit

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
-module.exports = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-}
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+ 
+};
+
+export default nextConfig;
